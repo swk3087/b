a62f0d4350716555903079be6ea0037093fea2b9
wonkyeom seo <89524975+swk3087@users.noreply.github.com>
2026-02-18 15:47:41 +0900

    Update keys.json.ex

--- keys.json.ex
+++ keys.json.ex
@@ -9,7 +9,7 @@
     "logQueryMaxLimit": 5000
   },
   "openai": {
-    "apiKey": "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",
+    "apiKey": "sk-proj-CQVZ6t7OpBCc_k8A",
     "model": "gpt-4o-mini"
   },
   "firebase": {
@@ -21,8 +21,8 @@
   },
   "firebaseAdmin": {
     "projectId": "schoo1-info",
-    "clientEmail": "firebase-adminsdk-nc5r0@schoo1-info.iam.gserviceaccount.com",
-    "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDMudsGrCNIp6N1\nvqeBYpAFgTf1btLdCtpBTCZ+EWpbBfshbCipl3wBTt6xplYhgjZL3SIDdymC1A3U\nKQsFpbSwjalreXNe4hOEAwpTA6cD74VimOTXW1C0uqHHcTvOY5pApIn4jrFcj775\npmJbHCOss07WqeUBiyl3kfsYHveNEdNdIAzSHvonLCuE5EAy+q5OOzleVyPdk1XM\nGT9zn6wFkqX4rSuEjWVkUZgX5FiZc5tJTQ9M+ymTHmcXWryySk1TRMCyxHTXTJL2\nOQiuCN0HGZFajSSnLs8pdYbDvC8lqPjRqqDBSY9fDtHij35pAp6Hn0gRDKtXuYC6\nTKiCzTI3AgMBAAECggEAFH1bdE5Wf7Hg1GrUT/OXOih7JcdwN5WbpM0OzdpHXJHQ\n1xZV9tdFnsnlKZQzavTMeLOf8Qcu8pGaSrbK4XH6uhrg2clU0yppopzn88lZ9gpg\naARTuabBgT0WmPlLxOByHUXP1ekjdiygLqieOm6ADDZWoyRrhPKHysL5Bap/wzCs\nv8G8GRE48COUEr0khcFvIOPyyEi2xJtuxGzRSTSR9NT5IzNO/t9G9+aV2Tp7j/H7\ni7dE3+LC0I5YXpFYCkF8hghrB2xMgOFR8iE3Dhwjxa6A3mkqPT/WHlw7dFskp0qx\nf6Cewx93bvna9RJ8tA6nAHrDtLICa3NOC+4PDbFuYQKBgQD2kCuBoaSMmGIUsRKq\nYtdoUlf3n3zOSh7uH4/v8Xt87XJlwhjQ2Zg2ugomMR4c5SqlBKhb9TpPSGXGu+wV\n0krzWQJFhBGepYy42iIApqjzn3Q1Ux+5olscA5Ab4aw3ZWP4FuVDKaXhZZhByR5y\nkUTzIVDU9uEJlapbqo1N6AiVVwKBgQDUj8OtmfmeLkTpqiIoWKAHWUMvKJIMxhMO\nw6Abxil/p5ZtJ7zJlQg4yUx++iBhMdqKs7cRHHtiueIwZfs5kBp1n2N9uBGv+Jq1\nl92CBZkEKpzgCT9Z3mmhMypbaTXvu6nej1OgGGoOm1vN6w6wJDzP1BDrS2/EH/zO\nvtXk3oReIQKBgCoATSj7/CfnHW4g6+kShVdED7Ai/wO1JKezwLVDEaIqYL5s3gVV\ny9QmxOvUmiiZ8TXDIVro31H6Jzw/buwYsqPZmcV1xfqcgOhMYFZ0DXvSwd0Dl/fr\nbFHNWZMmUkvtfaYZYFycj6FhyRoLxKAHlxNDrZ1ZoOQdFrQezYtj53JpAoGANmk2\npdGsXBlr+oRaigllrXvj/sZb3Hk3l6fr7DAln0++6eIqKU/E8wRUWIyejUK9tpOa\nYP7xdqwugkUYsft5SnKqfRwJKwpwT9u4D/hu2uotjgvP86lDUMhQJ2YfMGbRbO8z\ncN/PBj4umd95uw0LpAec7R46VfH6PNIM3rE3IQECgYAGSn8u/Hd4dv6OK6kbKsr3\nnwmgM051Gvl9ehnO0cmYTPwPbLGDtkJlHBwMCTgkc536wRPqG8/d790XmT1D7th0\n2eGWpHLUU1d2xZ+/E3peYxLne8U6NkN/GCY8iJ32np5HLnBcA+Mc1RGFIVLYGeuO\n2RmjD6z7EjAPy62wbg+big==\n-----END PRIVATE KEY-----\n"
+    "clientEmail": "firebiceaccount.com",
+    "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhk4hOEAwzTI3AgMBAAECggEAFH1bdE5Wf7Hg1GrUT/OXOih7JcdwN5WbpM0OzdpHXJHQ\n1xZV9tdFnsnlKZQzavTMeLOf8Qcu8pGaSrbK4XH6uhrg2clU0yppopzn88lZ9gpg\naARTuabBgT0WmPlLxOByHUXP1ekjdiygLqieOm6ADDZWoyRrhPKHysL5Bap/wzCs\nv8G8GRE48COUEr0khcFvIOPyyEi2xJtuxGzRSTSR9NT5IzNO/t9G9+aV2Tp7j/H7\ni7dE3+LC0I5YXpFYCkF8hghrB2xMgOFR8iE3Dhwjxa6A3mkqPT/WHlw7dFskp0qx\nf6Cewx93bvna9RJ8tA6nAHrDtLICa3NOC+4PDbFuYQKBgQD2kCuBoaSMmGIUsRKq\nYtdoUlf3n3zOSh7uH4/v8Xt87XJlwhjQ2Zg2ugomMR4c5SqlBKhb9TpPSGXGu+wV\n0krzWQJFhBGepYy42iIApqjzn3Q1Ux+5olscA5Ab4aw3ZWP4FuVDKaXhZZhByR5y\nkUTzIVDU9uEJlapbqo1N6AiVVwKBgQDUj8OtmfmeLkTpqiIoWKAHWUMvKJIMxhMO\nw6Abxil/p5ZtJ7zJlQg4yUx++iBhMdqKs7cRHHtiueIwZfs5kBp1n2N9uBGv+Jq1\nl92CBZkEKpzgCT9Z3mmhMypbaTXvu6nej1OgGGoOm1vN6w6wJDzP1BDrS2/EH/zO\nvtXk3oReIQKBgCoATSj7/CfnHW4g6+kShVdED7Ai/wO1JKezwLVDEaIqYL5s3gVV\ny9QmxOvUmiiZ8TXDIVro31H6Jzw/buwYsqPZmcV1xfqcgOhMYFZ0DXvSwd0Dl/fr\nbFHNWZMmUkvtfaYZYFycj6FhyRoLxKAHlxNDrZ1ZoOQdFrQezYtj53JpAoGANmk2\npdGsXBlr+oRaigllrXvj/sZb3Hk3l6fr7DAln0++6eIqKU/E8wRUWIyejUK9tpOa\nYP7xdqwugkUYsft5SnKqfRwJKwpwT9u4D/hu2uotjgvP86lDUMhQJ2YfMGbRbO8z\ncN/PBj4umd95uw0LpAec7R46VfH6PNIM3rE3IQECgYAGSn8u/Hd4dv6OK6kbKsr3\nnwmgM051Gvl9ehnO0cmYTPwPbLGDtkJlHBwMCTgkc536wRPqG8/d790XmT1D7th0\n2eGWpHLUU1d2xZ+/E3peYxLne8U6NkN/GCY8iJ32np5HLnBcA+Mc1RGFIVLYGeuO\n2RmjD6z7EjAPy62wbg+big==\n-----END PRIVATE KEY-----\n"
   },
   "webPush": {
     "subject": "mailto:admin@planriseai.kro.kr",
